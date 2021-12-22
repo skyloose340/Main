471ff3904a97c4c85ce8c7b2c5dd658ea6d544c1
skyloose340 <92951876+skyloose340@users.noreply.github.com>
2021-12-22 08:11:13 -0800

    Create Main\Skyloose\CST\skywars.lua

---
+++ Main\Skyloose\CST\skywars.lua
@@ -0,0 +1,533 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
+local Window = Library.CreateLib("SkyWars Master", "BloodTheme")
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Player Section")
+
+--Inf Jump 
+Section:NewToggle("Infinite Jump", "op jumping", function(state)
+    if state then
+        _G.infinjump = true
+
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
+    else
+        _G.infinjump = false
+
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
+    end
+end)
+
+--WalkSpeed
+
+Section:NewToggle("WalkSpeed", "sonic go brrrrr", function(state)
+    if state then
+        _G.WS = 40
+		local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+		Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+			Humanoid.WalkSpeed = _G.WS;
+		end)
+		Humanoid.WalkSpeed = _G.WS;
+    else
+        _G.WS = 16
+		local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+		Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+			Humanoid.WalkSpeed = _G.WS;
+		end)
+		Humanoid.WalkSpeed = _G.WS;
+    end
+end)
+
+-- Visuals Tab
+
+
+local Tab = Window:NewTab("Visuals")
+local Section = Tab:NewSection("Visuals Section")
+
+Section:NewButton("Box Esp", "", function()
+local Player = game:GetService("Players").LocalPlayer
+local Camera = game:GetService("Workspace").CurrentCamera
+local Mouse = Player:GetMouse()
+
+local function Dist(pointA, pointB) -- magnitude errors for some reason  : (
+    return math.sqrt(math.pow(pointA.X - pointB.X, 2) + math.pow(pointA.Y - pointB.Y, 2))
+end
+
+local function GetClosest(points, dest)
+    local min  = math.huge 
+    local closest = nil
+    for _,v in pairs(points) do
+        local dist = Dist(v, dest)
+        if dist < min then
+            min = dist
+            closest = v
+        end
+    end
+    return closest
+end
+
+local function DrawESP(plr)
+    local Box = Drawing.new("Quad")
+    Box.Visible = false
+    Box.PointA = Vector2.new(0, 0)
+    Box.PointB = Vector2.new(0, 0)
+    Box.PointC = Vector2.new(0, 0)
+    Box.PointD = Vector2.new(0, 0)
+    Box.Color = Color3.fromRGB(255, 255, 255)
+    Box.Thickness = 2
+    Box.Transparency = 1
+
+    local function Update()
+        local c
+        c = game:GetService("RunService").RenderStepped:Connect(function()
+            if plr.Character ~= nil and plr.Character:FindFirstChildOfClass("Humanoid") ~= nil and plr.Character:FindFirstChild("HumanoidRootPart") ~= nil and plr.Character:FindFirstChildOfClass("Humanoid").Health > 0 and plr.Character:FindFirstChild("Head") ~= nil then
+                local pos, vis = Camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
+                if vis then 
+                    local points = {}
+                    local c = 0
+                    for _,v in pairs(plr.Character:GetChildren()) do
+                        if v:IsA("BasePart") then
+                            c = c + 1
+                            local p = Camera:WorldToViewportPoint(v.Position)
+                            if v.Name == "HumanoidRootPart" then
+                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, 0, -v.Size.Z)).p)
+                            elseif v.Name == "Head" then
+                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(0, v.Size.Y/2, v.Size.Z/1.25)).p)
+                            elseif string.match(v.Name, "Left") then
+                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(-v.Size.X/2, 0, 0)).p)
+                            elseif string.match(v.Name, "Right") then
+                                p = Camera:WorldToViewportPoint((v.CFrame * CFrame.new(v.Size.X/2, 0, 0)).p)
+                            end
+                            points[c] = p
+                        end
+                    end
+                    local Left = GetClosest(points, Vector2.new(0, pos.Y))
+                    local Right = GetClosest(points, Vector2.new(Camera.ViewportSize.X, pos.Y))
+                    local Top = GetClosest(points, Vector2.new(pos.X, 0))
+                    local Bottom = GetClosest(points, Vector2.new(pos.X, Camera.ViewportSize.Y))
+
+                    if Left ~= nil and Right ~= nil and Top ~= nil and Bottom ~= nil then
+                        Box.PointA = Vector2.new(Right.X, Top.Y)
+                        Box.PointB = Vector2.new(Left.X, Top.Y)
+                        Box.PointC = Vector2.new(Left.X, Bottom.Y)
+                        Box.PointD = Vector2.new(Right.X, Bottom.Y)
+
+                        Box.Visible = true
+                    else 
+                        Box.Visible = false
+                    end
+                else 
+                    Box.Visible = false
+                end
+            else
+                Box.Visible = false
+                if game.Players:FindFirstChild(plr.Name) == nil then
+                    c:Disconnect()
+                end
+            end
+        end)
+    end
+    coroutine.wrap(Update)()
+end
+
+for _,v in pairs(game:GetService("Players"):GetChildren()) do
+    if v.Name ~= Player.Name then 
+        DrawESP(v)
+    end
+end
+
+game:GetService("Players").PlayerAdded:Connect(function(v)
+    DrawESP(v)
+end)
+
+end)
+
+Section:NewSlider("Fov", "Camra Fov", 120, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    workspace.CurrentCamera.FieldOfView  = s
+end)
+
+--ComBat Tab 
+
+local Tab = Window:NewTab("ComBat")
+local Section = Tab:NewSection("ComBat Section")
+Section:NewButton("Sword Range (equipe sword firt)", "Sword Range Control", function()
+ for i,v in pairs(game:GetService'Players'.LocalPlayer.Character:GetChildren())do
+                if v:isA("Tool") then
+                    local a = Instance.new("SelectionBox",v.Handle)
+                    a.Adornee = v.Handle
+                    v.Handle.Size = Vector3.new(0.5,0.5,60)
+                    v.GripPos = Vector3.new(0,0,0)
+                    lplayer.Character.Humanoid:UnequipTools()
+                end
+end
+end)
+
+--AutoFarms Tab
+local Tab = Window:NewTab("AutoFarms")
+local Section = Tab:NewSection("AutoFarms Section")
+Section:NewButton("AutoCoins click when round start equipe Axe", "op autocoins farm", function()
+local cpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+local stuff = workspace:getDescendants()
+for i=1,#stuff do
+if stuff[i].Name == "Block" and stuff[i].Parent.Name == "Ores" then
+repeat
+wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = stuff[i].CFrame
+game.Players.LocalPlayer.Character.Axe.RemoteEvent:FireServer(stuff[i])
+until stuff[i].Name ~= "Block" or not game.Players.LocalPlayer.Character:findFirstChild("Axe")
+end
+end
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cpos
+end)
+
+Section:NewButton("AutoFarm Win", "Get Many Wins", function(state)
+ 
+		local oh1 = CFrame.new(0.644594908, 264, 67.4945374)
+		local oh2 = game:GetService("Players")
+		local oh3 = oh2.LocalPlayer.Character.HumanoidRootPart
+
+end)
+
+local Tab = Window:NewTab("Teleports")
+local Section = Tab:NewSection("Teleports Section")
+
+Section:NewButton("Mega Vip", "go to mega vip", function()
+    local plr = game:GetService('Players').LocalPlayer.Character
+plr.HumanoidRootPart.CFrame = CFrame.new(12, 264, 79)
+end)
+
+Section:NewButton("Vip", "go to vip", function()
+local plr = game:GetService('Players').LocalPlayer.Character
+plr.HumanoidRootPart.CFrame = CFrame.new(-11, 264, -79)
+end)
+
+Section:NewButton("Shop", "go to Shop", function()
+local plr = game:GetService('Players').LocalPlayer.Character
+plr.HumanoidRootPart.CFrame = CFrame.new(60, 260.5, 0)
+end)
+
+Section:NewButton("Spawn Lobby", "go to spawn lobby", function()
+local oh1 = CFrame.new(-0.418475986, 268, -0.111892045)
+	local oh2 = game:GetService("Players")
+	local oh3 = oh2.LocalPlayer.Character.HumanoidRootPart
+
+
+	oh3.CFrame = oh1 
+end)
+-- Utilitys Tab
+local Tab = Window:NewTab("Utilitys")
+local Section = Tab:NewSection("Utilitys Section")
+Section:NewButton("Infinite Yield", "Launch Infinne Yield", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+Section:NewButton("Op combat Gui", "Launch Op combat Gui", function()
+local active = true
+local trueActive = true
+local reachType = "Sphere"
+local dmgEnabled = true
+local visualizerEnabled = false
+ 
+local visualizer = Instance.new("Part")
+visualizer.BrickColor = BrickColor.Blue()
+visualizer.Transparency = 0.6
+visualizer.Anchored = true
+visualizer.CanCollide = false
+visualizer.Size = Vector3.new(0.5,0.5,0.5)
+visualizer.BottomSurface = Enum.SurfaceType.Smooth
+visualizer.TopSurface = Enum.SurfaceType.Smooth
+ 
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local Frame_2 = Instance.new("Frame")
+local Frame_3 = Instance.new("Frame")
+local Frame_4 = Instance.new("Frame")
+local Frame_5 = Instance.new("Frame")
+local TextButton_2 = Instance.new("TextButton")
+ 
+--Properties:
+ 
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.DisplayOrder = 999999999
+ScreenGui.ResetOnSpawn = false
+ 
+Frame.Parent = ScreenGui
+Frame.AnchorPoint = Vector2.new(0, 0.5)
+Frame.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Frame.BackgroundTransparency = 0.300
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 4
+Frame.Position = UDim2.new(0, 0, 0.600000024, 0)
+Frame.Size = UDim2.new(0.150000006, 0, 0.300000012, 0)
+ 
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0.600000024, 0, 0.200000003, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Reach:"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+ 
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BackgroundTransparency = 1.000
+TextBox.Position = UDim2.new(0.600000024, 0, 0, 0)
+TextBox.Size = UDim2.new(0.400000006, 0, 0.200000003, 0)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.Text = "3.5"
+TextBox.TextColor3 = Color3.fromRGB(255, 76, 76)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+ 
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0, 0, 0.200000003, 0)
+TextLabel_2.Size = UDim2.new(0.600000024, 0, 0.200000003, 0)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Shape:"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+ 
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0, 0, 0.400000006, 0)
+TextLabel_3.Size = UDim2.new(0.600000024, 0, 0.200000003, 0)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Damage:"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+ 
+TextLabel_4.Parent = Frame
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.Position = UDim2.new(0, 0, 0.600000024, 0)
+TextLabel_4.Size = UDim2.new(0.600000024, 0, 0.200000003, 0)
+TextLabel_4.Font = Enum.Font.SourceSans
+TextLabel_4.Text = "Visualizer:"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 14.000
+TextLabel_4.TextWrapped = true
+ 
+TextButton.Parent = Frame
+TextButton.AnchorPoint = Vector2.new(0, 1)
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Position = UDim2.new(0, 0, 1, 0)
+TextButton.Size = UDim2.new(1, 0, 0.150000006, 0)
+TextButton.Font = Enum.Font.SourceSansBold
+TextButton.Text = "Kill Script"
+TextButton.TextColor3 = Color3.fromRGB(222, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+ 
+Frame_2.Parent = Frame
+Frame_2.Active = true
+Frame_2.AnchorPoint = Vector2.new(0, 0.5)
+Frame_2.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(0.725000024, 0, 0.699999988, 0)
+Frame_2.Size = UDim2.new(0, 25, 0, 25)
+Frame_2.ZIndex = 5
+ 
+Frame_3.Parent = Frame_2
+Frame_3.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame_3.BackgroundColor3 = Color3.fromRGB(255, 76, 76)
+Frame_3.BorderSizePixel = 0
+Frame_3.LayoutOrder = 1
+Frame_3.Position = UDim2.new(0.5, 0, 0.5, 0)
+ 
+Frame_4.Parent = Frame
+Frame_4.Active = true
+Frame_4.AnchorPoint = Vector2.new(0, 0.5)
+Frame_4.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+Frame_4.BorderSizePixel = 0
+Frame_4.Position = UDim2.new(0.725000024, 0, 0.5, 0)
+Frame_4.Size = UDim2.new(0, 25, 0, 25)
+Frame_4.ZIndex = 5
+ 
+Frame_5.Parent = Frame_4
+Frame_5.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame_5.BackgroundColor3 = Color3.fromRGB(255, 76, 76)
+Frame_5.BorderSizePixel = 0
+Frame_5.LayoutOrder = 1
+Frame_5.Position = UDim2.new(0.5, 0, 0.5, 0)
+Frame_5.Size = UDim2.new(1, 0, 1, 0)
+ 
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.BackgroundTransparency = 1.000
+TextButton_2.Position = UDim2.new(0.600000024, 0, 0.200000003, 0)
+TextButton_2.Size = UDim2.new(0.400000006, 0, 0.200000003, 0)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "Sphere"
+TextButton_2.TextColor3 = Color3.fromRGB(255, 76, 76)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14.000
+TextButton_2.TextWrapped = true
+ 
+repeat wait() until game.Players.LocalPlayer
+ScreenGui.Parent = game:GetService("CoreGui")
+ 
+ 
+ 
+Frame_4.InputBegan:connect(function(inp)
+	if inp.UserInputType == Enum.UserInputType.MouseButton1 then
+		dmgEnabled = not dmgEnabled
+		local goal = {Size = UDim2.new(0,0,0,0)}
+		if dmgEnabled then
+			goal = {Size = UDim2.new(1,0,1,0)}
+		end
+		game:GetService("TweenService"):Create(Frame_5,TweenInfo.new(0.12,Enum.EasingStyle.Quad),goal):Play()
+	end
+end)
+Frame_2.InputBegan:connect(function(inp)
+	if inp.UserInputType == Enum.UserInputType.MouseButton1 then
+		visualizerEnabled = not visualizerEnabled
+		local goal = {Size = UDim2.new(0,0,0,0)}
+		if visualizerEnabled then
+			goal = {Size = UDim2.new(1,0,1,0)}
+		end
+		game:GetService("TweenService"):Create(Frame_3,TweenInfo.new(0.12,Enum.EasingStyle.Linear),goal):Play()
+	end
+end)
+TextButton_2.MouseButton1Click:connect(function()
+	if reachType == "Sphere" then
+		reachType = "Line"
+	else
+		reachType = "Sphere"
+	end
+	TextButton_2.Text = reachType
+end)
+TextButton.MouseButton1Click:connect(function()
+	trueActive = false
+	ScreenGui:Destroy()
+end)
+game:GetService("UserInputService").InputBegan:connect(function(inp,gpe)
+	if gpe then return end
+	if inp.KeyCode == Enum.KeyCode.H then
+		ScreenGui.Enabled = not ScreenGui.Enabled
+	end
+end)
+ 
+local plr = game.Players.LocalPlayer
+ 
+local function onHit(hit,handle)
+    local victim = hit.Parent:FindFirstChildOfClass("Humanoid")
+	    if victim and victim.Parent.Name ~= game.Players.LocalPlayer.Name then
+		if dmgEnabled then
+	        for _,v in pairs(hit.Parent:GetChildren()) do
+	            if v:IsA("Part") then
+	                firetouchinterest(v,handle,0)
+	                firetouchinterest(v,handle,1)
+	            end
+			end
+		else
+			firetouchinterest(hit,handle,0)
+			firetouchinterest(hit,handle,1)
+		end
+    end
+end
+ 
+local function getWhiteList()
+    local wl = {}
+    for _,v in pairs(game.Players:GetPlayers()) do
+        if v ~= plr then
+            local char = v.Character
+            if char then
+                for _,q in pairs(char:GetChildren()) do
+                    if q:IsA("Part") then
+                        table.insert(wl,q)
+                    end
+                end
+            end
+        end
+    end
+    return wl
+end
+ 
+game:GetService("RunService").RenderStepped:connect(function()
+    if not active or not trueActive then return end
+	local s = plr.Character and plr.Character:FindFirstChildOfClass("Tool")
+	if not s then visualizer.Parent = nil end
+    if s then
+        local handle = s:FindFirstChild("Handle") or s:FindFirstChildOfClass("Part")
+		if handle then
+			if visualizerEnabled then
+				visualizer.Parent = workspace
+			else
+				visualizer.Parent = nil
+			end
+			local reach = tonumber(TextBox.Text)
+			if reach then
+				if reachType == "Sphere" then
+					visualizer.Shape = Enum.PartType.Ball
+					visualizer.Size = Vector3.new(reach,reach,reach)
+					visualizer.CFrame = handle.CFrame
+		            for _,v in pairs(game.Players:GetPlayers()) do
+		                local hrp = v.Character and v.Character:FindFirstChild("HumanoidRootPart")
+		                if hrp and handle then
+		                    local mag = (hrp.Position-handle.Position).magnitude
+		                    if mag <= reach then
+		                        onHit(hrp,handle)
+		                    end
+		                end
+					end
+				elseif reachType == "Line" then
+					local origin = (handle.CFrame*CFrame.new(0,0,-2)).p
+		    		local ray = Ray.new(origin,handle.CFrame.lookVector*-reach)
+					local p,pos = workspace:FindPartOnRayWithWhitelist(ray,getWhiteList())
+					visualizer.Shape = Enum.PartType.Block
+					visualizer.Size = Vector3.new(1,0.8,reach)
+					visualizer.CFrame = handle.CFrame*CFrame.new(0,0,(reach/2)+2)
+		    		if p then
+		    		    onHit(p,handle)
+		    		else
+		    		    for _,v in pairs(handle:GetTouchingParts()) do
+		    		        onHit(v,handle)
+		    		    end
+		    		end
+				end
+			end
+        end
+    end
+end)
+end)
